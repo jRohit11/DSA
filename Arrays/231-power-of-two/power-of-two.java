c9c867007d655ebafb7384f97d72c6287c7d6b75
Rohit Jha <86003711+jRohit11@users.noreply.github.com>
2025-06-27 16:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (79.43%) - LeetSync

--- Arrays/231-power-of-two/power-of-two.java
+++ Arrays/231-power-of-two/power-of-two.java
@@ -3,10 +3,12 @@ class Solution {
         if(n==1){
             return true;
         }
+        if(n<=0){
+            return false;
+        }
         if(n%2!=0){
             return false;
         }
-        if(n<=0) return false;
         return isPowerOfTwo(n/2);
     }
 }
