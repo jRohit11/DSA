8cca70314647aab8a60ecc0100f8e5e157d3fb6d
Rohit Jha <86003711+jRohit11@users.noreply.github.com>
2025-07-26 11:17:28 +0530

    Time: 13 ms (62.25%) | Memory: 54.8 MB (88.68%) - LeetSync

--- Arrays/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ Arrays/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -3,9 +3,9 @@ class Solution {
         return helper(nums,k)-helper(nums,k-1);
     }
     public int helper(int[] nums,int k){
-        int l=0,r=0;
         int oddCount=0;
-        int count=0;
+        int l=0,r=0;
+        int ans=0;
         while(r<nums.length){
             if(nums[r]%2!=0){
                 oddCount++;
@@ -16,9 +16,9 @@ class Solution {
                 }
                 l++;
             }
-            count+=r-l+1;
+            ans+=r-l+1;
             r++;
         }
-        return count;
+        return ans;
     }
 }
