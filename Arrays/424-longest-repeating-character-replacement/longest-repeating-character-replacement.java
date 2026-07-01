2c8c1980360b869390e853642e3d3479a4ddb642
Rohit Jha <86003711+jRohit11@users.noreply.github.com>
2026-07-02 00:20:13 +0530

    Time: 32 ms (17.63%) | Memory: 47.5 MB (7.28%) - LeetSync

--- Arrays/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ Arrays/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,23 +1,45 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+
         int l=0,r=0;
-        int maxF=0;
-        int maxL=0;
-        int changes=0;
-        int[] hash=new int[26];
+        int maxLen=0;
+        HashMap<Character,Integer> map =new HashMap<>();
+        int maxFreq=0;
+        int flip=0;
         while(r<s.length()){
-            hash[s.charAt(r)-'A']++;
-            maxF=Math.max(maxF,hash[s.charAt(r)-'A']);
-            changes=(r-l+1)-maxF;
-            while(changes>k){
-                hash[s.charAt(l)-'A']--;
-                maxF=Math.max(maxF,hash[s.charAt(l)-'A']);
+           
+            map.put(s.charAt(r),map.getOrDefault(s.charAt(r),0)+1);
+            maxFreq=Math.max(maxFreq,map.get(s.charAt(r)));
+            flip=(r-l+1)-maxFreq;
+
+            while(flip>k){
+                map.put(s.charAt(l),map.get(s.charAt(l))-1);
+                maxFreq=Math.max(maxFreq,map.get(s.charAt(l)));
                 l++;
-                changes=(r-l+1)-maxF;
+                flip=(r-l+1)-maxFreq;
+               
             }
-            maxL=Math.max(maxL,r-l+1);
+            maxLen=Math.max(maxLen,r-l+1);
             r++;
         }
-        return maxL;
+        return maxLen;
     }
+            // int maxLen=0;
+        // for(int i=0;i<s.length();i++){
+        //     HashMap<Character,Integer> map=new HashMap<>();
+        //     int maxFreq=0;
+        //     for(int j=i;j<s.length();j++){
+
+        //         map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
+        //         maxFreq=Math.max(maxFreq,map.get(s.charAt(j)));
+        //         int flip=(j-i+1)-maxFreq;
+
+        //         if(flip<=k){
+        //             maxLen=Math.max(maxLen,j-i+1);
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return maxLen;
 }
