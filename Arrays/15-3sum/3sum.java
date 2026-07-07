bbdebb9816646ef30345dc9346dff01d070d4285
Rohit Jha <86003711+jRohit11@users.noreply.github.com>
2026-07-07 22:14:57 +0530

    Time: 36 ms (30.99%) | Memory: 59.3 MB (32.89%) - LeetSync

--- Arrays/15-3sum/3sum.java
+++ Arrays/15-3sum/3sum.java
@@ -4,29 +4,32 @@ class Solution {
         Arrays.sort(nums);
         int n= nums.length;
         for(int i=0;i<n-2;i++){
-            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
-                int low = i+1;
-                int high = n-1;
-                int sum = 0-nums[i];
-                while(low<high){
-                    if(nums[low]+nums[high]==sum){
-                        ans.add(Arrays.asList(nums[i],nums[low],nums[high]));
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int l=i+1;
+            int r=n-1;
+            while(l<r){
 
-                        while(low<high && nums[low]==nums[low+1]){
-                            low++;
-                        }
-                        while(low<high && nums[high]==nums[high-1]){
-                            high--;
-                        }
-                        low++;
-                        high--;
-                    }else if(nums[low]+nums[high]<sum){
-                        low++;
-                    }else{
-                        high--;
+                if(nums[i]+nums[l]+nums[r]==0){
+                    ans.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1]){
+                        l++;
+                    }
+                    while(l<r && nums[r]==nums[r+1]){
+                        r--;
                     }
                 }
-            }       
+                else if(nums[i]+nums[l]+nums[r]>0){
+                    r--;
+                }
+                else{
+                    l++;
+                }
+
+            }
         }
         return ans;
     }
