7a0532115258f05a245a781761e4faa71720b208
Rohit Jha <86003711+jRohit11@users.noreply.github.com>
2026-07-13 23:24:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (54.46%) - LeetSync

--- Arrays/908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ Arrays/908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -15,7 +15,9 @@ class Solution {
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
+
         }
         return slow;
+        
     }
 }
