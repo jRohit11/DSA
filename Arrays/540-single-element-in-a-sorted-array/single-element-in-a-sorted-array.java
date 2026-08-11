1c2052281b759a2cbaafb1f82f3037b82c60e26b
Rohit Jha <86003711+jRohit11@users.noreply.github.com>
2026-08-11 22:46:32 +0530

    Time: 0 ms (100.00%) | Memory: 52.7 MB (78.06%) - LeetSync

--- Arrays/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ Arrays/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -9,17 +9,18 @@ class Solution {
         if(nums[nums.length-1]!=nums[nums.length-2]){
             return nums[nums.length-1];
         }
-        int low=1;
-        int high=nums.length-2;
-        while(low<=high){
-            int mid=low+(high-low)/2;
+        int left=1;
+        int right=nums.length-2;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
             else if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1])){
-                low=mid+1;
+                left=mid+1;
             }else{
-                high=mid-1;
+                right=mid-1;
             }
         }
         return -1;
